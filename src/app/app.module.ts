40aea2f2383cdd197ac92dcd59864d7f040c9d3a
henriqueganz <henrique.ganz@gmail.com>
2023-06-07 15:57:38 -0300

    My first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'crud-base-afcda',
+    appId: '1:389630583980:web:9bc5a3f618aa937a85ab09',
+    databaseURL: 'https://crud-base-afcda-default-rtdb.firebaseio.com',
+    storageBucket: 'crud-base-afcda.appspot.com',
+    apiKey: 'AIzaSyDUdhlwfjNQst_1AedKp3weoja87TkUHeU',
+    authDomain: 'crud-base-afcda.firebaseapp.com',
+    messagingSenderId: '389630583980',
+  },
+};
