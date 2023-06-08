3195bfe2394000fc86330313e5ce6bcf2567bb62
henriqueganz <henrique.ganz@gmail.com>
2023-06-08 16:40:29 -0300

    page component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonComponent } from './button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NoteComponent } from './pages/note/note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    LoginComponent,
+    HomeComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
